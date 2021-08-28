f834bb4083c1c018902c651467d5beb27efb52fd
hemalawati <root@Hemas-MacBook-Pro.local>
2021-08-27 22:23:12 -0400

    added google map api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,28 @@
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./styles";
+
 const Map = () => {
-    return (
-        <div>
-            <h1>Map</h1>
-        </div>
-    );
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAdFCa0kK29y6rqgvuXX85z7xFjR6gjA5U" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onnChildChange={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
 export default Map;
